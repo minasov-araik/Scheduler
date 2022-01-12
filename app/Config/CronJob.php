890aeb5707f3a1dbf6affd8d98e0a12983bdabaf
minasov-araik <minasov.araik@tftc.dev>
2022-01-12 14:39:10 +0400

    debug

--- app/Config/CronJob.php
+++ app/Config/CronJob.php
@@ -67,7 +67,7 @@ class CronJob {
     {
         $schedule->call(function () {
 
-            file_put_contents('text.txt', 'test data time ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
+            //file_put_contents('text.txt', 'test data time ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
 
         })->everyMinute()->named('foo');
 
